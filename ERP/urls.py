5db06a8533b134fe69c285306b1831ab9d8aba50
Shine Mathew <shinemathew24@gmail.com>
2022-11-14 19:30:11 +0530

    name changed

--- ERP/urls.py
+++ ERP/urls.py
@@ -1,4 +1,4 @@
-"""ERP_School URL Configuration
+"""ERP URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
